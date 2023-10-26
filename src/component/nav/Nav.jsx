755f0a8b7c1f4dec2abcaa41d9963a82c63a697e
Mohit <mohit.devade123@gmail.com>
2023-10-26 18:08:36 +0530

    Flooting Nav bar added

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsTools} from 'react-icons/bs'
+import {BiCodeAlt} from 'react-icons/bi'
+import {BiMessageAltDetail} from 'react-icons/bi'
+
+
 const Nav = () => {
+  const [activeNav,setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={()=>setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav==='#about' ? 'active':''} ><AiOutlineUser/></a>
+      <a href="#skills" onClick={()=>setActiveNav('#skills')} className={activeNav==='#skills' ? 'active':''}><BsTools/></a>
+      <a href="#portfolio" onClick={()=>setActiveNav('#portfolio')} className={activeNav==='#portfolio' ? 'active':''}><BiCodeAlt/></a>
+      <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav==='#contact' ? 'active':''}><BiMessageAltDetail/></a>
+    </nav>
   )
 }
 
