be320be18b9188e810e291fbf1bfa6d34d265cdc
Mohit <mohit.devade123@gmail.com>
2023-10-27 14:41:32 +0530

    Project component added

---
+++ src/component/projects/Projects.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./projects.css";
+import Data from "./projects_data";
+
+console.log(Data);
+const Portfolio = () => {
+  return (
+    <section id="projects">
+      <h5>My Recent work</h5>
+      <h2>Porjects</h2>
+
+      <div className="container project__container">
+        {Data.map(({ id, title, image, github, demo }) => {
+          return (
+            <article key={id} className="project__item">
+              <div className="project__item-image">
+                <img src={image} alt="" />
+              </div>
+              <h3>{title}</h3>
+              <div className="project__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  GitHub
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
---
+++ src/component/projects/projects_data.js
@@ -0,0 +1,30 @@
+import IMG1 from "../../assests/project-1.png";
+import IMG2 from "../../assests/project-2.jpg";
+import IMG3 from "../../assests/project-3.jpg";
+
+const Data = [
+    {
+        id:1,
+        title:"Image Gallary App",
+        image:IMG1,
+        github:'https://github.com/mohit162001/Image_app_react',
+        demo:'https://dynamic-search-app.vercel.app/'
+    },
+    {
+        id:2,
+        title:"Travel Companion Website",
+        image:IMG2,
+        github:'https://github.com/ritik2727/Travel-Companion-frontend',
+        demo:'https://travel-companion-tau.vercel.app/'
+    },
+    {
+        id:3,
+        title:"Char Pahiya E-Commerce",
+        image:IMG3,
+        github:'https://github.com/ritik2727/OnlineCarsSelling-mern',
+        demo:'https://char-pahiya.vercel.app/'
+    },
+    
+]
+
+export default Data;
