9464c64e955ba068365461701962876e17061b42
Mohit <mohit.devade123@gmail.com>
2023-10-27 14:41:08 +0530

    component name portfolio to projects

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
       <a href="#" onClick={()=>setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav==='#about' ? 'active':''} ><AiOutlineUser/></a>
       <a href="#skills" onClick={()=>setActiveNav('#skills')} className={activeNav==='#skills' ? 'active':''}><BsTools/></a>
-      <a href="#portfolio" onClick={()=>setActiveNav('#portfolio')} className={activeNav==='#portfolio' ? 'active':''}><BiCodeAlt/></a>
+      <a href="#projects" onClick={()=>setActiveNav('#projects')} className={activeNav==='#projects' ? 'active':''}><BiCodeAlt/></a>
       <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav==='#contact' ? 'active':''}><BiMessageAltDetail/></a>
     </nav>
   )
